cf89d9a03f0f8dcac2f374b47071f89dc11768d5
vaghelarajeshh <vaghela.rajeshh@gmail.com>
2020-08-30 18:47:57 -0400

    Update date and time commands

--- Hello.py
+++ Hello.py
@@ -1,7 +1,27 @@
 import sys
-import datetime
+from datetime import datetime
 
-date = (date.datetime)
+date = datetime.now()
 
-print ("Today dat is $date.",date)
+print ("Today date is ",date)
 print ("Hello World ")
+
+curr_datetime = datetime.now()
+
+curr_year = curr_datetime.year
+curr_month = curr_datetime.month
+curr_day = curr_datetime.day
+
+curr_hour = curr_datetime.hour
+curr_min = curr_datetime.minute
+curr_sec = curr_datetime.second
+
+print(f"The current date time is {curr_datetime}")
+
+print(f"The year is {curr_year}")
+print(f"The month is {curr_month}")
+print(f"The day is {curr_day}")
+
+print(f"Current hour of the day is {curr_hour}")
+print(f"Current minute of the day is {curr_min}")
+print(f"Current seconds are {curr_sec}")
